4f8b8cd4df9ef6ad748e0773ae8ad0aa6abf6b8e
Rajatrai1212 <rajatrai1212@gmail.com>
2022-11-13 14:06:59 +0530

    learned file system CRUD and async

--- filesys.js
+++ filesys.js
@@ -1,1 +1,22 @@
-lg
+const fs  = require('fs')
+
+//fs.writeFileSync('apple.txt','this is apple file hahaha')
+//this creates a file in the open dir not in a folder
+
+//using this in path module with a loop
+
+const path = require('path')
+const dirPath = path.join(__dirname,'files')
+console.warn(dirPath)
+
+// for(i=0;i<5;i++)
+// {
+//     fs.writeFileSync(dirPath+`/hello${i}.txt`,"created a file")
+// }
+fs.readdir(dirPath,(err,files)=>{
+  
+    files.forEach((item)=>{
+        console.log("file name is ",item)
+
+    })
+})
