ddc15eacbb4fad60e231f918b9a3a1d09079eb20
Rajatrai1212 <rajatrai1212@gmail.com>
2022-11-14 20:22:11 +0530

    learned- get method,made 404 page,removed file extension from url

---
+++ htmliNnode.js
@@ -0,0 +1,22 @@
+
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const publicPath = path.join(__dirname,'public')
+
+// app.use(express.static(publicPath))
+app.get("/",(req,res)=>{
+
+    res.sendFile(`${publicPath}/home.html`)
+})
+app.get("/about",(req,res)=>{
+
+    res.sendFile(`${publicPath}/about.html`)
+})
+app.get("*",(req,res)=>{
+
+    res.sendFile(`${publicPath}/noPage.html`)
+})
+
+app.listen(5000)
